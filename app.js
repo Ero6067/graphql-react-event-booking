daf4c8f6aa2bca726fb6eef91b657612807eff43
Kris Lafrance <Kris.lafrance6067@gmail.com>
2020-01-04 02:27:03 -0800

    adding mongodb connection

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+
+const Event = require("./models/events.js");
 
 const app = express();
 
@@ -46,19 +49,39 @@ app.use(
 				return events;
 			},
 			createEvent: args => {
-				const event = {
-					_id: Math.random().toString(),
+				const event = new Event({
 					title: args.eventInput.title,
 					description: args.eventInput.description,
 					price: +args.eventInput.price,
-					date: args.eventInput.date
-				};
-				events.push(event);
-				return event;
+					date: new Date(args.eventInput.date)
+				});
+				return event
+					.save()
+					.then(result => {
+						console.log(result);
+						return { ...result._doc };
+					})
+					.catch(err => {
+						console.log(err);
+						throw err;
+					});
 			}
 		},
 		graphiql: true
 	})
 );
+
+mongoose
+	.connect(
+		`mongodb+srv://${process.env.MONGO_USER}:
+  ${process.env.MONGO_PASSWORD}
+  @cluster0-vwdht.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+	)
+	.then(() => {
+		app.listen(3000);
+	})
+	.catch(err => {
+		console.log(err);
+	});
 
 app.listen(3000);
