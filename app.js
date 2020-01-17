724728790859d0188a8d80608407070659651b00
Kris Lafrance <Kris.lafrance6067@gmail.com>
2020-01-17 00:42:27 -0800

    able to create and login with user from front end

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ const isAuth = require("./middleware/is-auth");
 
 const app = express();
 
+//prevents CORS errors
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	if (req.method === "OPTIONS") {
+		return res.sendStatus(200);
+	}
+	next();
+});
+
 app.use(bodyParser.json());
 
 app.use(isAuth);
--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -3,11 +3,21 @@ import React, { Component } from "react";
 import "./Auth.css";
 
 class AuthPage extends Component {
+	state = {
+		isLogin: true
+	};
+
 	constructor(props) {
 		super(props);
 		this.emailElem = React.createRef();
 		this.passwordElem = React.createRef();
 	}
+
+	switchModeHandler = () => {
+		this.setState(prevState => {
+			return { isLogin: !prevState.isLogin };
+		});
+	};
 
 	submitHandler = event => {
 		event.preventDefault();
@@ -18,7 +28,50 @@ class AuthPage extends Component {
 			return;
 		}
 
-		console.log(email, pw);
+		let reqBody = {
+			query: `
+        query {
+          login(email: "${email}", password: "${pw}") {
+            userId
+            token
+            tokenExpiration
+          }
+        }
+      `
+		};
+
+		if (!this.state.isLogin) {
+			reqBody = {
+				query: `
+          mutation {
+            createUser(userInput: { email: "${email}", password: "${pw}"}) {
+              _id
+              email
+            }
+          }
+        `
+			};
+		}
+
+		fetch("http://localhost:8000/graphql", {
+			method: "POST",
+			body: JSON.stringify(reqBody),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(res => {
+				if (res.status !== 200 && res.status !== 201) {
+					throw new Error("Failed");
+				}
+				return res.json();
+			})
+			.then(resData => {
+				console.log(resData);
+			})
+			.catch(err => {
+				console.log(err);
+			});
 	};
 
 	render() {
@@ -34,7 +87,9 @@ class AuthPage extends Component {
 				</div>
 				<div className="form-actions">
 					<button type="submit">Submit</button>
-					<button type="button"> Switch to Signup</button>
+					<button type="button" onClick={this.switchModeHandler}>
+						Switch to {this.state.isLogin ? "Signup" : "Login"}
+					</button>
 				</div>
 			</form>
 		);
